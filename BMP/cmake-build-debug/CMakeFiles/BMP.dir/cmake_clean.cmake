dab232832fe8a994bb3d125105c2edac7ec2b9d7
ThanhTuyen <contact@tuyen.me>
2019-03-16 17:36:20 +0700

    build a program doing some mapulations on a BMP file

---
+++ BMP/cmake-build-debug/CMakeFiles/BMP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BMP.dir/main.c.o"
+  "BMP.pdb"
+  "BMP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BMP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
