7b9307787eeea21ecd1cbd5271424f58bce21a49
Tuyen Nguyen <contact@tuyen.me>
2019-07-29 00:26:43 +0700

    done with reading bmp1 from file, continuing for create bmp2 as requirement

--- BMP/main.c
+++ BMP/main.c
@@ -58,13 +58,14 @@ void readBmpFile(FILE* fp, BitMapHeader* header, Dib* bmif, RGB* color) {
     fseek(fp, sizeof(BitMapHeader), 0);
     fread(&bmif,sizeof(struct DIB),1,fp);
 
+
     printf("\n%s",header->bfType);
     printf("\n%s",header->bfSize);
     printf("\n%s",bmif->biSize);
     printf("\n%d",bmif->biWidth);
     printf("\n%d",bmif->biHeight);
 
-    if (header.bfType != bmSign)
+    if (header->bfType != bmSign)
     {
         printf("Khong phai file bitmap");
     }
@@ -72,17 +73,46 @@ void readBmpFile(FILE* fp, BitMapHeader* header, Dib* bmif, RGB* color) {
 
 }
 
+void scanBmpPixelLine(FILE* fp, RGB* color, uint32_t lengthColumn){
+    if (!fp)
+        return;
 
+    color = (RGB*) malloc (sizeof(RGB)*lengthColumn);
+
+    fread(color, sizeof(RGB), lengthColumn, fp);
+}
+
+void skipPadding(FILE* fp, char count){
+    if (!fp)
+        return;
+
+    if (count == 0)
+        return;
+
+    char* padding = NULL;
+    padding = (char*) malloc(3);
+
+    fread(padding, count, 1, fp);
+}
 
 void readPixelArray(FILE* fp, BitMapHeader bmhd, Dib bmif, PixelArray* data){
 //read het cac thong so de nhap vao bien pixel
     if (!fp)
         return;
+
     (*data).rowCount = bmif.biHeight;
     (*data).columnCount = bmif.biWidth;
     (*data).pixel = (RGB**) malloc (bmif.biHeight*(sizeof(RGB*)));
+
+    //Phan can cong voi chieu rong de la boi so cua 4
     char paddingCount = (4 - (bmif.biWidth) * ((int)bmif.biBitCount/8) % 4) % 4;
 
+    fseek(fp, (long) bmhd.bfOffBits, SEEK_SET);
+
+    for (int i = 0; i < (bmif.biHeight); i++) {
+        scanBmpPixelLine(fp, data->pixel[bmif.biHeight - i - 1], bmif.biWidth);
+        skipPadding(fp, paddingCount);
+    }
 }
 
 
@@ -111,35 +141,37 @@ void readFromFile(BitMap* bmp) {
     readPixelArray(buffer, bmp->header, bmp->bmif, &bmp->pixel);
 }
 
-void main() {
-    FILE* fp1 = NULL;
+void drawBmp(BitMap* bmp1){
 
+    BitMap* bmp2;
+    bmp2 = (BitMap*) malloc (sizeof(BitMap));
+    //Sau do tiep tuc tao ra 1 file BMP hien tren console theo yeu cau de bai:
+    //Nhap vao 1 con so bat ky tu -100 den 100, chinh do sang toi cua anh:
+}
 
-
-    struct Bitmap* bmp1;
+int main() {
+    //Khoi tao 1 file BMP de nhan du lieu file bmp ban dau tu link
+    BitMap* bmp1;
+    bmp1 = (BitMap*) malloc (sizeof(BitMap));
 
     readFromFile(bmp1);
 
-    //fseek(fp,0,SEEK_SET);
+    drawBmp(bmp1);
 
-    FILE* fp2 = NULL;
+   /* FILE* fp2 = NULL;
     fp2 = fopen("D:\\bitmap.out","w");
 
     if(!fp2){
         printf("Open file 2 fail ...");
-        return;
+        return
     }
-
-
-//Nhap vao 1 con so bat ky tu -100 den 100, chinh do sang toi cua anh
-
-
     fclose(fp1);
     fclose(fp2);
     getchar();
 
     HWND console = GetConsoleWindow();
     HDC hdc = GetDC(console);
+    */
 
     return
 }
